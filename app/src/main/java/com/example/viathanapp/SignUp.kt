273fd0d10d65366b04fd98dce83566934195f0e3
Joseph Deborah Oluwabunmi <josephdeborah506@gmail.com>
2021-01-02 21:13:49 +0100

    new changes

---
+++ app/src/main/java/com/example/viathanapp/SignUp.kt
@@ -0,0 +1,11 @@
+package com.example.viathanapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+    }
+}
