dda49a3885e33656335f34d73da243cd2339e980
Joseph Deborah Oluwabunmi <josephdeborah506@gmail.com>
2021-01-09 03:49:57 +0100

    new changes

--- app/src/main/java/com/example/viathanapp/MainActivity.kt
+++ app/src/main/java/com/example/viathanapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.viathanapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sign_in.setOnClickListener(){
+            startActivity(Intent (this, SignIn:: class.java))
+        }
+
+        sign_up.setOnClickListener(){
+            startActivity(Intent (this, SignUp:: class.java))
+        }
     }
 }
--- app/src/main/java/com/example/viathanapp/SignIn.kt
+++ app/src/main/java/com/example/viathanapp/SignIn.kt
@@ -1,11 +1,17 @@
 package com.example.viathanapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.sign_in.*
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_in)
+
+
+    log_in.setOnClickListener(){
+        startActivity(Intent (this, Dashboard:: class.java))
     }
-}
+}}
