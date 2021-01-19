d6ea451516a664dd7a0929a124dd60a5460e4aef
Joseph Deborah Oluwabunmi <josephdeborah506@gmail.com>
2021-01-19 20:18:22 +0100

    new changes

--- app/src/main/java/com/example/viathanapp/Dashboard.kt
+++ app/src/main/java/com/example/viathanapp/Dashboard.kt
@@ -1,11 +1,15 @@
 package com.example.viathanapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        menu_bar.setOnClickListener(){ startActivity(Intent(this, Dashboard::class.java ))
+        }
     }
 }
