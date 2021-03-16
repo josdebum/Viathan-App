c669aa21fd4fefd42ecbd9164e1e486eef64356b
Joseph Deborah Oluwabunmi <josephdeborah506@gmail.com>
2021-03-16 21:27:47 +0100

    new changes

--- app/src/main/java/com/example/viathanapp/EachMessage.kt
+++ app/src/main/java/com/example/viathanapp/EachMessage.kt
@@ -7,13 +7,39 @@ import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.PopupWindow
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.github.rifqimfahmi.softkeyboard.widget.MenuEditText
+import com.github.rifqimfahmi.softkeyboard.widget.SoftKeyBoardPopup
 import kotlinx.android.synthetic.main.activity_each_message.*
+import kotlinx.android.synthetic.main.activity_each_message.menuChatContainer
 
-class EachMessage : AppCompatActivity() {
+class EachMessage : AppCompatActivity() , MenuEditText.PopupListener{
+    lateinit var editText: MenuEditText
+    lateinit var menuKeyboard: SoftKeyBoardPopup
+    lateinit var rootView: ConstraintLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_each_message)
+
+        editText = findViewById(R.id.editText)
+
+        editText.popupListener = this
+
+        menuKeyboard = SoftKeyBoardPopup(
+            this,
+            rootView,
+            editText,
+            editText,
+            menuChatContainer
+        )
+
+        menu_chat.setOnClickListener {
+            toggle()
+        }
 
 
 
@@ -24,7 +50,13 @@ class EachMessage : AppCompatActivity() {
         }
     }
 
-
+    private fun toggle() {
+        if (menuKeyboard.isShowing) {
+            menuKeyboard.dismiss()
+        } else {
+            menuKeyboard.show()
+        }
+    }
 
         override fun onCreateOptionsMenu(menu: Menu): Boolean {
             val inflater = menuInflater
@@ -67,4 +99,15 @@ class EachMessage : AppCompatActivity() {
         onBackPressed()
         return true
     }
+
+
+
+    override fun onDestroy() {
+        menuKeyboard.clear()
+        super.onDestroy()
     }
+
+    override fun getPopup(): PopupWindow {
+        return menuKeyboard
+    }
+}
