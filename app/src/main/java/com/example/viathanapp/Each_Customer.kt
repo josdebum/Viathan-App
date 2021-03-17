612c5d535b358e4a1fe377920fc1e85cb815afe7
Joseph Deborah Oluwabunmi <josephdeborah506@gmail.com>
2021-03-18 00:17:30 +0100

    new changes

---
+++ app/src/main/java/com/example/viathanapp/Each_Customer.kt
@@ -0,0 +1,11 @@
+package com.example.viathanapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Each_Customer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_each__customer)
+    }
+}
